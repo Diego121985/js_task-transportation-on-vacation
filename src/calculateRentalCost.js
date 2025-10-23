13073150d888c4721106784ba4b2ee0d55a01844
Diego121985 <diegoleite59986@gmail.com>
2025-10-22 21:10:26 -0300

    test4

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -9,7 +9,7 @@ function calculateRentalCost(days) {
   const MIN_DAYS_FOR_SMALL_DISCOUNT = 3;
   const MIN_DAYS_FOR_BIG_DISCOUNT = 7;
 
-  let total = days * DAILY_RATE;
+  const total = days * DAILY_RATE;
 
   if (days >= MIN_DAYS_FOR_BIG_DISCOUNT) {
     return total - DISCOUNT_DAYS_7;
